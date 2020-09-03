1cbe848a9937920c5119cb6c9655853615266d37
slowtacocar <13561297+slowtacocar@users.noreply.github.com>
2020-09-02 21:45:44 -0400

    Use SVG instead of CSS

--- GameServer.js
+++ GameServer.js
@@ -5,20 +5,20 @@ module.exports = class GameServer {
     this.io = io
     this.games = {}
   }
-  
+
   joinPlayer(room, socket) {
     if (!this.games[room]) {
       this.games[room] = new Game(room, this.io)
     }
-    
+
     return this.games[room].addPlayer(socket)
   }
-  
-  start() {    
-    this.io.on('connection', (socket) => {      
+
+  start() {
+    this.io.on('connection', (socket) => {
       socket.on('join', (room, ack) => {
         const component = this.joinPlayer(room, socket)
-        
+
         ack(component)
       })
     })
--- components/Game.js
+++ components/Game.js
@@ -1,6 +1,8 @@
 import styles from "./Game.module.css";
 
 export default function Game(props) {
+  const svg = React.useRef();
+
   const paddles = {
     "paddle1": {
       "x": 20,
@@ -43,31 +45,31 @@ export default function Game(props) {
   function handleMouseMove(event) {
     event.persist();
 
+    let mouse = svg.current.createSVGPoint()
+    mouse.x = event.clientX
+    mouse.y = event.clientY
+
+    mouse = mouse.matrixTransform(svg.current.getScreenCTM().inverse())
+
     props.socket.emit(props.name, {
-      "x": event.clientX,
-      "y": event.clientY
+      "x": mouse.x,
+      "y": mouse.y
     });
 
     setThisPaddle((thisPaddle) => ({
       "x": thisPaddle.x,
-      "y": Math.min(475, Math.max(25, event.clientY))
+      "y": Math.min(475, Math.max(25, mouse.y))
     }))
   }
 
   return (
-    <div className={styles.game} onMouseMove={handleMouseMove}>
-      <div className={styles.paddle} style={{
-        "top": thisPaddle.y,
-        "left": thisPaddle.x
-      }} />
-      <div className={styles.paddle} style={{
-        "top": remotePaddle.y,
-        "left": remotePaddle.x
-      }} />
-      <div className={styles.ball} style={{
-        "top": ball.y,
-        "left": ball.x
-      }} />
+    <div onMouseMove={handleMouseMove}>
+      <svg className={styles.svg} viewBox="-1 -1 502 502" ref={svg}>
+        <rect width="501" height="501" x="-0.5" y="-0.5" stroke="rgb(255, 255, 255)" />
+        <rect width="10" height="50" x={thisPaddle.x - 5} y={thisPaddle.y - 25} fill="rgb(255, 255, 255)" />
+        <rect width="10" height="50" x={remotePaddle.x - 5} y={remotePaddle.y - 25} fill="rgb(255, 255, 255)" />
+        <circle cx={ball.x} cy={ball.y} r="15" fill="rgb(255, 255, 255)" />
+      </svg>
     </div>
   );
 }
