4ec99154a3fac7b613bb584363c44104768be8b4
Bobby <slowtacocar@gmail.com>
2021-02-07 12:59:11 -0500

    Change host for socket.io

--- pages/[room].js
+++ pages/[room].js
@@ -2,7 +2,7 @@ import { useRouter } from "next/router";
 import io from "socket.io-client";
 import Lobby from "../components/Lobby";
 
-const socket = io();
+const socket = io("https://pong-server-slowtacocar.herokuapp.com");
 
 export default function Home() {
   const router = useRouter();
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import io from "socket.io-client";
 import Lobby from "../components/Lobby";
 
-const socket = io();
+const socket = io("https://pong-server-slowtacocar.herokuapp.com/");
 
 export default function Home() {
   return <Lobby socket={socket} />;
